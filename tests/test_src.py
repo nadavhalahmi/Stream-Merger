f4c8d5d6c625c8e42e1fd26840abfa96cbfabd47
Nadav Halahmi <nadavhalahmi560@gmail.com>
2022-08-21 22:52:13 +0300

    added support for fixed data size as input

--- tests/test_src.py
+++ tests/test_src.py
@@ -1,5 +1,10 @@
 from src import *
 
 
-def test_basic(basic_input: bytes, basic_sync: bytes, basic_output: bytes) -> None:
-    assert translate(basic_input, basic_sync) == basic_output
+def test_basic(basic_input: bytes, basic_sync: bytes, basic_output: bytes, basic_data_size: int) -> None:
+    assert translate(basic_input, basic_sync, basic_data_size) == basic_output
+
+
+def test_ignore_first_bytes(input_with_prefix: bytes, basic_sync: bytes, basic_output: bytes, basic_data_size: int) -> None:
+    assert translate(input_with_prefix, basic_sync,
+                     basic_data_size) == basic_output
