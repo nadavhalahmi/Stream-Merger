012511898db0fc1e248dc0aa2efbc73a07a4d810
Nadav Halahmi <nadavhalahmi560@gmail.com>
2022-08-22 18:45:42 +0300

    fixed FixedTranslator implementation to pass test_input_is_sync

--- src/Translators.py
+++ src/Translators.py
@@ -6,15 +6,25 @@ class FixedTranslator:
     def __init__(self, sync: bytes, data_size: int):
         self.sync: bytes = sync
         self.data_size: int = data_size
+        self.sync_size = len(sync)
+        self.message_size = self.sync_size + data_size
         self.input_so_far: bytes = b''
 
     def translate(self, input_bytes: bytes) -> List[bytes]:
         self.input_so_far += input_bytes
-        splited_so_far: List[bytes] = self.input_so_far.split(self.sync)[1:]
-        splited_so_far = list(
-            filter(lambda x: x, map(
-                lambda s: s[:self.data_size], splited_so_far)))
-        return splited_so_far
+        if self.message_size > len(self.input_so_far):
+            return []  # input_so_far can't hold a full message
+        window = (0, self.sync_size)
+        res: List[bytes] = []
+        while window[1] + self.data_size - 1 < len(self.input_so_far):
+            if self.input_so_far[window[0]:window[1]] == self.sync:
+                res.append(
+                    self.input_so_far[window[1]:window[1] + self.data_size])
+                window = (window[0] + self.message_size,
+                          window[1] + self.message_size)
+            else:
+                window = (window[0] + 1, window[1] + 1)
+        return res
 
 
 class OffsetTranslator:
