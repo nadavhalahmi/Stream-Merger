982960ccdcfb7a8e5060b3f933e6f16bbbb161a1
Nadav Halahmi <nadavhalahmi560@gmail.com>
2022-08-22 20:06:27 +0300

    fixed EndseqTranslator implementation. passed test_basic_2_inputs_2_outpus test

--- src/Translators.py
+++ src/Translators.py
@@ -1,5 +1,4 @@
 from typing import List
-import re
 
 
 class Translator:
@@ -66,10 +65,28 @@ class EndseqTranslator(Translator):
     def __init__(self, sync: bytes, endseq: bytes):
         super().__init__(sync)
         self.endseq: bytes = endseq
-        self.pattern: bytes = self.sync+b'(.*)'+self.endseq
+        self.endseq_size: int = len(self.endseq)
 
     def translate(self, input_bytes: bytes) -> List[bytes]:
         self.input_so_far += input_bytes
-        splited_so_far: List[bytes] = re.findall(
-            self.pattern, self.input_so_far)
-        return splited_so_far
+        if self.sync_size + self.endseq_size > len(self.input_so_far):
+            return []  # input_so_far can't even hold sync+endseq, surly can't hold data too
+        sync_window = (0, self.sync_size)
+        res: List[bytes] = []
+        while sync_window[1] <= len(self.input_so_far):
+            if self.input_so_far[sync_window[0]:sync_window[1]] == self.sync:
+                endseq_window = (
+                    sync_window[1], sync_window[1]+self.endseq_size)
+                while endseq_window[1] <= len(self.input_so_far):
+                    if self.input_so_far[endseq_window[0]:endseq_window[1]] == self.endseq:
+                        data = self.input_so_far[sync_window[1]                                                 :endseq_window[0]]
+                        res.append(data)
+                        break
+                    else:
+                        endseq_window = (
+                            endseq_window[0] + 1, endseq_window[1] + 1)
+                sync_window = (endseq_window[1],
+                               endseq_window[1] + self.sync_size)
+            else:
+                sync_window = (sync_window[0] + 1, sync_window[1] + 1)
+        return res
