e543269f4a083a8302e87b6b245f43024173ccc1
Nadav Halahmi <nadavhalahmi560@gmail.com>
2022-08-25 12:19:45 +0300

    fixed typing in test_basic_2_inputs_2_outpus

--- tests/test_endseq.py
+++ tests/test_endseq.py
@@ -7,6 +7,6 @@ def test_endseq(endseq_translator: EndseqTranslator, endseq_input: bytes, basic_
         endseq_input) == [basic_output]
 
 
-def test_basic_2_inputs_2_outpus(endseq_translator: OffsetTranslator, endseq_input: bytes, basic_output: bytes) -> None:
+def test_basic_2_inputs_2_outpus(endseq_translator: EndseqTranslator, endseq_input: bytes, basic_output: bytes) -> None:
     assert endseq_translator.translate(endseq_input) == [basic_output]
     assert endseq_translator.translate(endseq_input) == [basic_output]*2
