664198cefdb1849d8b861ba263fea863d01e7b5d
Nadav Halahmi <nadavhalahmi560@gmail.com>
2022-08-25 14:37:50 +0300

    fixed typo and removed unnecessary import

--- tests/test_endseq.py
+++ tests/test_endseq.py
@@ -1,4 +1,3 @@
-from src import *
 from src.Translators import EndseqTranslator
 
 
@@ -7,7 +6,7 @@ def test_endseq(endseq_translator: EndseqTranslator, endseq_input: bytes, basic_
         endseq_input) == [basic_output]
 
 
-def test_basic_2_inputs_2_outpus(endseq_translator: EndseqTranslator, endseq_input: bytes, basic_output: bytes) -> None:
+def test_basic_2_inputs_2_outputs(endseq_translator: EndseqTranslator, endseq_input: bytes, basic_output: bytes) -> None:
     assert endseq_translator.translate(endseq_input) == [basic_output]
     assert endseq_translator.translate(endseq_input) == [basic_output]*2
 
