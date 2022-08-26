7a0153b892637186bd0ddb19ec11c82fc15b0cb9
Nadav Halahmi <nadavhalahmi560@gmail.com>
2022-08-26 14:16:46 +0300

    cleaner tests

--- tests/test_endseq.py
+++ tests/test_endseq.py
@@ -17,15 +17,16 @@ def test_sync_is_endseq(endseq_translator_sync_is_endseq: EndseqTranslator, basi
     assert endseq_translator_sync_is_endseq.translate(message) == [simple_data]
 
 
-def test_random_long(endseq_translator: EndseqTranslator, rand_data: bytes, rand_bytes: bytes) -> None:
+def test_random_long(endseq_translator: EndseqTranslator, rand_bytes_no_sync: bytes) -> None:
     many_times = 10
     sync = endseq_translator.sync
     endseq = endseq_translator.endseq
-    full_message = rand_data + (sync + rand_data + endseq)*many_times
+    full_message = rand_bytes_no_sync + \
+        (sync + rand_bytes_no_sync + endseq)*many_times
     window_start = 0
     output = []
     while full_message[window_start: window_start+10]:
         output = endseq_translator.translate(
             full_message[window_start: window_start+10])
         window_start += 10
-    assert output == [rand_data]*many_times
+    assert output == [rand_bytes_no_sync]*many_times
